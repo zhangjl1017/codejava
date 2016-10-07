a5e1298d71ca56effee123a032e9ae367ff42e13
zhangjl1017 <ithlw@qq.com>
2016-10-07 12:41:32 +0800

    Git test

---
+++ test06/src/main/java/com/testGit/TestGit.java
@@ -0,0 +1,10 @@
+package com.testGit;
+
+/**
+ * Created by NIT on 2016/10/7.
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
